c9b3772ce4a6dca306f4427bd244a7b94cf972d1
mayuri <mayuri@mayuri-VirtualBox>
2020-08-19 12:09:12 +0530

    add_comment function added by developer1

--- final in class/src/final in class.cpp
+++ final in class/src/final in class.cpp
@@ -10,7 +10,16 @@
 #include"header.h"
 using namespace std;
 
+void add_comment()
+{
+
+	string str1= "developer 1 doing code";
+
+	cout<<"developer 1 code  = " << str1 <<endl;
+}
+
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	add_comment();
 	return 0;
 }
